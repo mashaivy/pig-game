8fcb0607f2b141e209ebdd812feecc437809fc2d
Student <example@example.com>
2019-08-28 16:58:54 -0700

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+$("form#number").submit(function(event){
+  event.preventDefault();
+  var input = parseInt($("input").val())
+  console.log(input)
+});
+
+
+for(var i=0; i<=input; i++) {
+  if i.toString().includes("1") {
+    score.push(input);
+  };
+  console.log("Hi");
+}
+
+
+
+
+
+
+var score = []
+
+});
